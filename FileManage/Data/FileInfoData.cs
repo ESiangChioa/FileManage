135e0e4f24824d5b896a497d146fa5ee256da7c7
zhaoxianrong <zhaoxianrong@fosafer.com>
2021-02-24 10:43:04 +0800

    first submit

---
+++ FileManage/Data/FileInfoData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FileManage.Data
+{
+    public class FileInfoData
+    {
+        public DateTime Date { get; set; }
+
+        public string Name { get; set; }
+
+        public string FilePath { get; set; }
+
+    }
+}
---
+++ FileManage/Data/FileInfoService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FileManage.Data
+{
+    public class FileInfoService
+    {
+        public Task<FileInfoData[]> GetForecastAsync(string softName)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                List<FileInfoData> fileInfoDatas = new List<FileInfoData>();
+
+                string path = Environment.CurrentDirectory + "//Resouses//" + softName;
+
+                if(!Directory.Exists(path))
+                    return fileInfoDatas.ToArray();
+                var files = Directory.GetFiles(path);
+                foreach (var file in files)
+                {
+                    FileInfo info = new FileInfo(file);
+                    FileInfoData fileInfo = new FileInfoData
+                    {
+                        Date = info.CreationTime,
+                        Name = info.Name,
+                        FilePath = $"Resouses/{softName}/{info.Name}"
+                    };
+                    fileInfoDatas.Add(fileInfo);
+                }
+
+                return fileInfoDatas.ToArray();
+            });
+        }
+    }
+}
